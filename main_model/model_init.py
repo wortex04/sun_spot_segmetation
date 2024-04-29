ff115a871836f9ef3c56a9af6876fc5916b4f556
wortex04 <marselbayramov04@gmail.com>
2024-04-29 23:27:48 +0300

    upgrate app

--- main_model/model_init.py
+++ main_model/model_init.py
@@ -1,9 +1,17 @@
-
 from main_model.u_net_model import U_Net
+import requests
 import torch
-def model_initification(path='main_model/w4_io_0.0619078509885223_loss_0.1604776674260696data2024-04-09 12_17_13.325687.pth'):
+import io
+
+
+def model_initification(path='https://storage.yandexcloud.net/marsel1/w2_io_loss_0.48305392265319824_loss_0.3506365865468979data2024-04-29%2010_34_50.416048.pth'):
+    # Send a GET request to download the file
+    response = requests.get(path, stream=True)
+    # Create a seekable buffer from the response content
+    buffer = io.BytesIO(response.content)
+    # Load the model weights from the buffer
+    model_weights = torch.load(buffer, map_location='cpu')
     model = U_Net()
-    model.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
+    model.load_state_dict(model_weights)
     model.eval()
     return model
-model_initification()
--- u_net_app.py
+++ u_net_app.py
@@ -9,22 +9,22 @@ import numpy as np
 import torch.nn.functional as F
 
 # Инициализация модели
-model = model_initification()
+import streamlit as st
+
+@st.cache
+def model_initification_cache():
+    return model_initification()
+model = model_initification_cache()
 
 # Переменная для сохранения предсказаний
 saved_predictions = None
 
 count = 0
 def predict(image):
-    global count
-    count += 1
     with torch.no_grad():
         masked, out, x, _ = model.infer(image)
-    mask = np.zeros((600, 600, 3), dtype=np.int16) + 255
-    for i in range(600):
-        for j in range(600):
-            if out[i, j] == 1:
-                mask[i, j] = 0
+    mask = np.ones((600, 600, 3), dtype=np.int16) * 255
+    mask[out == 1] = 0
     return (masked, mask, x)
 
 def get_base64(bin_file):
@@ -44,38 +44,38 @@ def set_background(png_file):
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-def main():
-    global saved_predictions, count
-    set_background('data_example/background.jpg')
+set_background('data_example/background.jpg')
 
-    st.title("Spot Detection")
-    uploaded_file = st.file_uploader("Выберите изображение", type=["jpg", "jpeg", "png"])
+st.title("Spot Detection")
+uploaded_file = st.file_uploader("Выберите изображение", type=["jpg", "jpeg", "png"])
+status = True
+if uploaded_file is not None:
+    image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+    last_uploaded_file = uploaded_file
 
-    if uploaded_file is not None:
-        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-
+    # Check if we already have a prediction for this image
+    if 'prediction' not in st.session_state or st.session_state['prediction'] is None:
         saved_predictions = predict(image.copy())
-        new_prediction_button_clicked = True
-        option = st.selectbox("Выберите что показать", ["masked", "mask", "x"])
-        col1, col2 = st.columns(2)
-        threshold = st.slider("Порог", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
-        with col1:
-            st.image(image, caption=f'Uploaded Image', use_column_width=True)
+        st.session_state['prediction'] = saved_predictions
+    else:
+        saved_predictions = st.session_state['prediction']
+    option = st.selectbox("Выберите что показать", ["masked", "mask", "x"])
+    col1, col2 = st.columns(2)
+    threshold = st.slider("Порог", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+    with col1:
+        st.image(image, caption=f'Uploaded Image', use_column_width=True)
 
-        with col2:
-            if option == "masked":
-                st.image(saved_predictions[0], caption='Masked Image', use_column_width=True)
-            elif option == "mask":
-                st.image(saved_predictions[1], caption='Mask', use_column_width=True)
-            elif option == "x":
-                x = F.softmax(saved_predictions[2])[0].squeeze(0)
+    with col2:
+        if option == "masked":
+            st.image(saved_predictions[0], caption='Masked Image', use_column_width=True)
+        elif option == "mask":
+            st.image(saved_predictions[1], caption='Mask', use_column_width=True)
+        elif option == "x":
+            x = F.softmax(saved_predictions[2])[0].squeeze(0)
 
-                mask = np.where(x > threshold, 255, 0)
-                mask = np.stack([mask, mask, mask], axis=0).transpose(1, 2, 0)
-                st.image(mask, caption='X', use_column_width=True)
+            mask = np.where(x > threshold, 255, 0)
+            mask = np.stack([mask, mask, mask], axis=0).transpose(1, 2, 0)
+            st.image(mask, caption='X', use_column_width=True)
 
 
-
-if __name__ == "__main__":
-    main()
