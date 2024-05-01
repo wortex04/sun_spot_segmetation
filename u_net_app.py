02c96fabf27cdee6c140597a12082c80c9815d82
wortex04 <marselbayramov04@gmail.com>
2024-05-01 10:45:04 +0300

    up app

--- u_net_app.py
+++ u_net_app.py
@@ -1,17 +1,13 @@
 import base64
-
-import streamlit as st
 import cv2
-import matplotlib.pyplot as plt
 import torch
 from main_model.model_init import model_initification
 import numpy as np
 import torch.nn.functional as F
-
-# Инициализация модели
 import streamlit as st
 
-@st.cache
+# Инициализация модели
+@st.cache_resource()
 def model_initification_cache():
     return model_initification()
 model = model_initification_cache()
@@ -44,41 +40,69 @@ def set_background(png_file):
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
+
+
+
 set_background('data_example/background.jpg')
 
 st.title("Spot Detection")
 uploaded_file = st.file_uploader("Выберите изображение", type=["jpg", "jpeg", "png"])
-status = True
-if uploaded_file is not None:
-    image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+
+# Список предложенных изображений
+default_images = ['sun_example/s_20151217.png', 'sun_example/s_20170809.png', 'sun_example/s_20180309.png']
+
+# Проверяем, было ли выбрано изображение из списка примеров
+image_selected_from_default = st.session_state.get('image_selected_from_default', False)
+
+if uploaded_file is None and not image_selected_from_default:
+    default_image_option = st.selectbox("Выберите изображение", default_images)
+    image_path = default_image_option
+    file_hash = image_path
+    image = cv2.imread(image_path)
     image = cv2.resize(image, (600, 600))
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Выбранное изображение', use_column_width=True)
+    st.session_state['default_image_option'] = default_image_option
+    st.session_state['image_selected_from_default'] = True
+else:
+    if not image_selected_from_default:
+        st.session_state['default_image_option'] = None
+    image_selected_from_default = False
 
-    # Вычисляем хэш загруженного файла
-    file_hash = uploaded_file.name
-    if 'last_file_hash' not in st.session_state or st.session_state['last_file_hash'] != file_hash:
-        # Файл изменился, выполняем предсказание
-        st.session_state['last_file_hash'] = file_hash
-        st.session_state['prediction'] = predict(image.copy())
-        saved_predictions = st.session_state['prediction']
+    if uploaded_file is not None:
+        image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
+        image = cv2.resize(image, (600, 600))
+        file_hash = uploaded_file.name
+        st.image(image, caption='Загруженное изображение', use_column_width=True)
     else:
-        # Файл не изменился, используем сохраненные предсказания
-        saved_predictions = st.session_state['prediction']
+        # Используем сохраненное изображение по умолчанию
+        default_image_option = st.session_state.get('default_image_option')
+        image_path = default_image_option
+        image = cv2.imread(image_path)
+        image = cv2.resize(image, (600, 600))
+        st.image(image, caption='Выбранное изображение', use_column_width=True)
+        file_hash = image_path
 
-    option = st.selectbox("Выберите что показать", ["masked", "mask", "x"])
-    col1, col2 = st.columns(2)
-    threshold = st.slider("Порог", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+if 'last_file_hash' not in st.session_state or st.session_state['last_file_hash'] != file_hash:
+    st.session_state['last_file_hash'] = file_hash
+    st.session_state['prediction'] = predict(image.copy())
+    saved_predictions = st.session_state['prediction']
+else:
+    saved_predictions = st.session_state['prediction']
 
-    with col1:
-        st.image(image, caption=f'Uploaded Image', use_column_width=True)
+option = st.selectbox("Выберите что показать", ["masked", "mask", "x"])
+col1, col2 = st.columns(2)
+threshold = st.slider("Порог", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
 
-    with col2:
-        if option == "masked":
-            st.image(saved_predictions[0], caption='Masked Image', use_column_width=True)
-        elif option == "mask":
-            st.image(saved_predictions[1], caption='Mask', use_column_width=True)
-        elif option == "x":
-            x = F.softmax(saved_predictions[2])[0].squeeze(0)
-            mask = np.where(x > threshold, 255, 0)
-            mask = np.stack([mask, mask, mask], axis=0).transpose(1, 2, 0)
-            st.image(mask, caption='X', use_column_width=True)
+with col1:
+    st.image(image, caption=f'Uploaded Image', use_column_width=True)
+
+with col2:
+    if option == "masked":
+        st.image(saved_predictions[0], caption='Masked Image', use_column_width=True)
+    elif option == "mask":
+        st.image(saved_predictions[1], caption='Mask', use_column_width=True)
+    elif option == "x":
+        x = F.softmax(saved_predictions[2])[0].squeeze(0)
+        mask = np.where(x > threshold, 255, 0)
+        mask = np.stack([mask, mask, mask], axis=0).transpose(1, 2, 0)
+        st.image(mask, caption='X', use_column_width=True)
